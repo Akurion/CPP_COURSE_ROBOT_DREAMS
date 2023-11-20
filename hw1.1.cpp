e229f5674fdf3364580684fb51fbaad1b6286bca
Akurion <akuraforever17@gmail.com>
2023-11-20 21:43:01 +0200

    Initial commit: First version

---
+++ hw1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+    
+    cout << "i love C++" << endl;
+    int height;
+
+    cout << "Enter triangle's height: "; 
+    cin >> height;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
